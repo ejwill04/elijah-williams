0f60f22ac48ce7319c942b3d8e298c5198fce6be
Elijah Williams <ejwill04@gmail.com>
2017-02-14 12:30:15 -0700

    Format projects

--- app/components/Projects/index.js
+++ app/components/Projects/index.js
@@ -6,9 +6,11 @@ const Projects = () => {
     <div className='projects-section'>
       <h2>projects</h2>
       <div className='project-info-wrapper'>
+
         <div className='project'>
-          <h3>Frogger</h3>
+          <h3 className='project-name'>Frogger</h3>
           <a
+            className='project-link'
             target='_blank'
             href='https://github.com/ejwill04/frogger'
             >github repo
@@ -18,35 +20,44 @@ const Projects = () => {
             src={bluecarlouisville} />
         </div>
 
-        <h3>filmFavs</h3>
-        <a
-          target='_blank'
-          href='https://github.com/ejwill04/movie-tracker'
-          >github repo
-        </a>
-        <img
-          className='project-snapshot'
-          src={bluecarlouisville} />
+        <div className='project'>
+          <h3 className='project-name'>filmFavs</h3>
+          <a
+            className='project-link'
+            target='_blank'
+            href='https://github.com/ejwill04/movie-tracker'
+            >github repo
+          </a>
+          <img
+            className='project-snapshot'
+            src={bluecarlouisville} />
+        </div>
 
-        <h3>Shoot Thou Breeze</h3>
-        <a
-          target='_blank'
-          href='https://github.com/ejwill04/shoot-the-breeze'
-          >github repo
-        </a>
-        <img
-          className='project-snapshot'
-          src={bluecarlouisville} />
+        <div className='project'>
+          <h3 className='project-name'>Shoot Thou Breeze</h3>
+          <a
+            className='project-link'
+            target='_blank'
+            href='https://github.com/ejwill04/shoot-the-breeze'
+            >github repo
+          </a>
+          <img
+            className='project-snapshot'
+            src={bluecarlouisville} />
+        </div>
 
-        <h3>Smart Budget</h3>
-        <a
-          target='_blank'
-          href='https://github.com/JeffBuss/smart-budget'
-          >github repo
-        </a>
-        <img
-          className='project-snapshot'
-          src={bluecarlouisville} />
+        <div className='project'>
+          <h3 className='project-name'>Smart Budget</h3>
+          <a
+            className='project-link'
+            target='_blank'
+            href='https://github.com/JeffBuss/smart-budget'
+            >github repo
+          </a>
+          <img
+            className='project-snapshot'
+            src={bluecarlouisville} />
+        </div>
       </div>
     </div>
   );
